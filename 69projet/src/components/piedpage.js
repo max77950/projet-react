b5c071a2e20ddf2ff11294ef9694c2c3fc835e72
max77950 <58215709+max77950@users.noreply.github.com>
2020-01-18 19:47:47 +0100

    Create piedpage.js

---
+++ 69projet/src/components/piedpage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './components.css';
+import univlogo from '/Users/maxencederoeck/Documents/GitHub/projet-react/69projet/src/image/logouniv.png'
+
+
+
+export default function PiedPage() {
+  
+
+  return (
+    
+
+<form id="pp">
+
+    <img src={univlogo} id='ppuniv'/>
+
+
+  
+  
+
+
+  
+
+</form>
+
+
+    
+    
+  );
+}
